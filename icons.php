f058530c2a9028d28427aab2fc959c09eedf9859
Varsha1277 <53369153+Varsha1277@users.noreply.github.com>
2021-06-05 18:50:43 +0530

    Add files via upload

---
+++ icons.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300&family=Limelight&family=Playfair+Display&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@700&family=Source+Sans+Pro:wght@300&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="mystyle.css">
+
+<body>
+    <div class="top-icons">
+
+
+  <div class="icon-bar-left">
+    <a href="#"><img src="logo.png" alt="Logo" width="60" height="50"></a>
+<hr class="hrline">
+  </div>
+  <div class="iconres">
+  <a class="active" href="#">RESUME</a>
+  </div>
+
+<div class="word"> Vars <span  style="color:#00cccc">ha Ra</span>mesh.<br> <span class="word1"> I'm A Passionate about <span  style="color:#00cccc;font-weight:bold">Developing  and Programming  </span> from India. </div>
+
+
+<div class="icon-bar-bleft">
+  <a class="active" href="#"><i class="fa fa-home"style="color:#00cccc;font-size:25px;"></i></a>
+  <a href="#aboutid"><i class="fa fa-id-badge"style="color:#00cccc;font-size:20px;"></i></a>
+
+  <a href="#"><i class="fa fa-envelope"style="color:#00cccc;font-size:20px;" ></i></a>
+  <a href="#"><i class="fa fa-phone" style="color:#00cccc;font-size:20px;"></i></a>
+  <a href="#"><i class="fa fa-window-maximize"style="color:#00cccc;font-size:20px;"></i></a>
+</div>
+
+<div class="icon-bar-bright">
+<a class="active" href="#"><i class="fa fa-facebook circle-icon"style="color:#00cccc;"></i></a>
+<a href="#"><i class="fa fa-twitter circle-icon"style="color:#00cccc;"></i></a>
+<a href="#"><i class="fa fa-envelope circle-icon"style="color:#00cccc;"></i></a>
+<a href="#"><i class="fa fa-linkedin circle-icon"style="color:#00cccc;"></i></a>
+<a href="#"><i class="fa fa-whatsapp circle-icon"style="color:#00cccc;"></i></a>
+</div>
+<div class="icon">
+<a class="active" href="#">Get in Touch</a>
+</div>
+<div class="verticalLine">sine</div>
+</div>
+<div id="aboutid">
+  <div class="about-word"> ABOUT ME</div>
+  <hr class="hrline1">
+  <div class="aboutpara"><img src="pineapple.jpg" alt="Pineapple" style="width:170px;height:170px;margin-left:15px;">Hello! My name is R.VARHSA and I enjoy creating things that live on the internet.<br> My interest in web development started back in 2019 taught me a lot about<br> HTML & CSS!
+ I am very passionate about programming<br> and Developing as a whole.<br>
+ <span class="para-next">I primarily work on the frontend projects and  I am very fond of complex systems<br> that require critical thinking skills and logic.
+ Though, I am flexible <br> and have worked in the Backend as well!</span>
+</div>
+</div>
+<div id="project-id">
+  <div class="project-word">Some Things I’ve Built </div>
+    <hr class="hrline2">
+    <div class="project-para"><a href="#"><img class="img-pro1" src="project1.png" alt="Pineapple" style="width:650px;height:300px;margin-left:15px;"></a></div>
+    <div class="project-exp">A web app for visualizing personalized Netflix based on Farming.It basically focuses on teach farmers through modern techniques.
+  It is fundamentally developed to support the farmers of to get resources related to their Business more effectively and easily
+</div>
+</div>
+<div id="Contact-id">
+  <div class="Contact-word"> CONTACT </div>
+    <hr class="hrline3">
+    <div class="contact-para">
+Get In Touch<br>I'm  currently looking for any new opportunities,<br> my inbox is always open. Whether you have a question or just want to say hi, I'll try my best to get back to you!
+
+    </div>
+</div>
+</body>
+</html>
